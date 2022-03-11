1a7062c15b6cf200decdf76885f64c587f188c57
xiraynedev <xiraynedev@gmail.com>
2022-03-11 15:26:14 -0500

    add get / route

--- src/server.ts
+++ src/server.ts
@@ -11,10 +11,14 @@ app.use(cors({
   origin: '*'
 }));
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Ready to make requests!');
+});
+
 app.get('/weather-api', async (req: Request, res: Response) => {
   const {data} = await axios.get(
     `https://api.openweathermap.org/data/2.5/weather?zip=${req.query.searchTerm}&appid=${process.env.WEATHER_KEY}`);
-  res.send(data)
+  res.send(data);
 });
 
 app.listen(3000, () => console.log(`Listening on port 3000...`));
