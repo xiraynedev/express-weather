84f124b094b24d3cf7e73cc7b2727ca3145d6fa9
xiraynedev <xiraynedev@gmail.com>
2022-03-11 15:25:10 -0500

    modify cors origin

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 require('dotenv').config();
 
 app.use(cors({
-  origin: 'http://localhost:8080'
+  origin: '*'
 }));
 
 app.get('/weather-api', async (req: Request, res: Response) => {
